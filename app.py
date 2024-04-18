2b85d951df3299559a48d71322b45a3c0f25ecf7
8Yanrin <Benny9112@yahoo.com.tw>
2024-04-18 12:59:01 +0800

    test commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+# from flask import Flask, render_template, request, redirect
+# app = Flask(__name__)
 
-@app.route("/hi")
-def hi():
-    return "hi"
+# @app.route("/hi")
+# def hi():
+#     return "hi"
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+print("test")
