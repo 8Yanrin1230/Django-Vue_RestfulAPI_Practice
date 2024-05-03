1fcc88a6c6bc7d3e14cb5bdd43e3f09a88b510a4
8Yanrin1230 <167327321+8Yanrin1230@users.noreply.github.com>
2024-05-03 10:23:30 +0800

    feat:open_robot

---
+++ backend/src/test.py
@@ -0,0 +1,9 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DcRobot.settings')
+import django
+django.setup()
+
+from DC.DcRobot import bot
+
+if __name__ == '__main__':
+    bot.run('MTIxNjgwMTg4MTQzOTg2MzAzNQ.GQgb05.pQNFZwT8dhc4hqy8hAPARYtPPMeaNUxa_wh4Aw')
